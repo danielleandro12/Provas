1164f968ffda03cdd6ed4fa6ec3f8c555c01a821
Daniel Leandro <166567188+danielleandro12@users.noreply.github.com>
2024-12-27 13:45:37 -0300

    Add files via upload

---
+++ Condicionais/prova.py
@@ -0,0 +1,7 @@
+numero= int(input("Digite um numero:"))
+if numero > 0 :
+    print(f"O {numero} é positivo")
+elif numero < 0 :
+    print(f"O {numero} é negativo")
+else:
+    print(f"O {numero} é zero")
